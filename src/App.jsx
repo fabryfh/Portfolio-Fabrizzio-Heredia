2654b0768985c2093582f17ec8920685c391ea43
Fabrizzio Heredia <134027194+fabryfh@users.noreply.github.com>
2023-08-21 16:35:30 -0500

    Linkeado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
 import ProyectsPage from './pages/ProyectsPage'
 import ProfilePage from './pages/ProfilePage'
 import TranslatePage from './pages/TranslatePage'
 import Page404 from './pages/Page404'
 import NavBar from './pages/NavBar'
 import MainInfo from './components/MainInfo'
+import ContactPage from './pages/ContactPage'
 
 
 
@@ -16,13 +16,14 @@ function App() {
     <div>
       <NavBar/>
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
+        <Route path='/' element={<MainInfo/>}/>
         <Route path='/profile' element={<ProfilePage/>}/>
-        <Route path='/proyects' element={<ProyectsPage/>}/>
+        <Route path='/projects' element={<ProyectsPage/>}/>
         <Route path='/translate' element={<TranslatePage/>}/>
+        <Route path='/contact' element={<ContactPage/>}/>
         <Route path='/*' element={<Page404/>}/>
       </Routes>
-      <MainInfo/>
+      
     </div>
   )
 }
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,13 +1,15 @@
 import "./styles/NavBar.css";
+import { Link } from "react-router-dom";
+
 
 const NavBar = () => {
   return (
     <article className="navBar__father__container">
       <section className="navBar__container">
         <div className="navBar__title">
-          <h1>
-            Desarrollador FullStack
-          </h1>
+          <Link to='/'>
+          <h1>Desarrollador FullStack</h1>
+          </Link>
           <div className="navBar__btn__container">
             <button className="navBar__btn">
               <span className="material-symbols-outlined">translate</span>
@@ -20,10 +22,18 @@ const NavBar = () => {
           </div>
         </div>
         <ul className="navBar__list__container">
+          <Link to='/profile'>
           <li className="navBar__list">About me</li>
+          </Link>
+          <Link to='/projects'>
           <li className="navBar__list">Projects</li>
+          </Link>
+          <Link to='/translate'>
           <li className="navBar__list">Languages</li>
+          </Link>
+          <Link to='/contact'>
           <li className="navBar__list">Contact</li>
+          </Link>
         </ul>
       </section>
     </article>
