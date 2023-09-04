05dc84362dc6141c82f31d532ab37af4a77f24de
Fabrizzio Heredia <134027194+fabryfh@users.noreply.github.com>
2023-09-04 04:15:34 -0500

    Contact Card

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import './styles/ContactPage.css'
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className="form__container">
+    <form className="form" >
+      <h1 className="heading">Contacto</h1>
+      <div>
+        <input
+          className="input"
+          placeholder="contact.name"
+          
+          type="text"
+          id="firstname"
+        />
+      </div>
+      <div>
+        <input
+          className="input"
+          placeholder="contact.lastname"
+        
+          type="text"
+          id="lastname"
+        />
+      </div>
+      <div>
+        <input
+          className="input"
+          placeholder="contact.email"
+          
+          type="email"
+          id="email"
+        />
+      </div>
+      <div>
+        <input
+          className="input"
+          placeholder="contact.phone"
+          
+          type="text"
+          id="phone"
+        />
+      </div>
+      <button className="btn">("contact.sendto")</button>
+    </form>
+  </div>
   )
 }
 
