47c7c201ed231b3b572179266d0633f3e2a94cb7
Fabrizzio Heredia <134027194+fabryfh@users.noreply.github.com>
2023-09-04 04:50:17 -0500

    Update contact form

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,51 +1,47 @@
-import React from 'react'
-import './styles/ContactPage.css'
+import React from "react";
+import "./styles/ContactPage.css";
 
 const ContactPage = () => {
   return (
     <div className="form__container animated pulse">
-    <form className="form" >
-      <h1 className="heading">Contacto</h1>
-      <div>
-        <input
-          className="input"
-          placeholder="Nombre completo"
-          
-          type="text"
-          id="firstname"
-        />
-      </div>
-      <div>
-        <input
-          className="input"
-          placeholder="Tema del proyecto"
-        
-          type="text"
-          id="lastname"
-        />
-      </div>
-      <div>
-        <input
-          className="input"
-          placeholder="Correo"
-          
-          type="email"
-          id="email"
-        />
-      </div>
-      <div>
-        <input
-          className="input"
-          placeholder="Número telefónico"
-          
-          type="text"
-          id="phone"
-        />
-      </div>
-      <button className="btn">Enviar</button>
-    </form>
-  </div>
-  )
-}
+      <form className="form">
+        <h1 className="heading">Contacto</h1>
+        <div>
+          <input
+            className="input"
+            placeholder="Nombre"
+            type="text"
+            id="name"
+          />
+        </div>
+        <div>
+          <input
+            className="input"
+            placeholder="Apellido"
+            type="text"
+            id="lastname"
+          />
+        </div>
+        <div>
+          <input
+            className="input"
+            placeholder="Número telefónico"
+            type="text"
+            id="phone"
+          />
+        </div>
+        <div>
+          <input
+            className="input"
+            placeholder="Correo"
+            type="email"
+            id="email"
+          />
+        </div>
+        <button className="btn">Enviar</button>
+      </form>
+    </div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
